88d707cc9ed92a5c53ac35521178aa3ce7e419f6
amazingguni <amazingguni@gmail.com>
2016-03-14 22:35:48 +0900

    task를 추가하는 selenium testcase추가

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class CommonTest(unittest.TestCase):
 	def setUp(self):
@@ -9,9 +10,21 @@ class CommonTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_site_title(self):
+	def test_add_task(self):
 		self.browser.get('http://localhost:8000')
-		self.assertIn('Pomodoro', self.browser.title)
+		self.assertIn('Pomodoro web', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'The task to do')
+		inputbox.send_keys('write code')
+		inputbox.send_keys(Keys.ENTER)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text=='1: write code' for row in rows))
+
+		self.fail('Finish the test')
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
