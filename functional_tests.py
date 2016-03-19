fafc508801e54d4d13b37b0b5eb34e10e5f05e4e
amazingguni <amazingguni@gmail.com>
2016-03-19 13:54:20 +0900

    템플릿 사용

--- functional_tests.py
+++ functional_tests.py
@@ -15,7 +15,7 @@ class CommonTest(unittest.TestCase):
 		self.assertIn('Pomodoro web', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
-
+		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'The task to do')
 		inputbox.send_keys('write code')
@@ -23,7 +23,6 @@ class CommonTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(any(row.text=='1: write code' for row in rows))
-
 		self.fail('Finish the test')
 
 if __name__ == '__main__':
