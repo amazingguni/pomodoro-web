fc7fb0cc649b28263fbbff61e1c19ab158038313
amazingguni <amazingguni@gmail.com>
2016-03-19 15:53:52 +0900

    item을 task로 수정, Task model 생성

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from tasks.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from tasks.models import Task
 
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
@@ -20,14 +21,32 @@ class HomePageTest(TestCase):
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
 		request.method = 'POST'
-		request.POST['item_text'] = 'new task'
+		request.POST['task_text'] = 'new task'
 
 		response = home_page(request)
 
 		expected_html = render_to_string(
 			'home.html',
-			{'new_item_text':'new task'},
+			{'new_task_text':'new task'},
 			request=request
 		)
 		self.assertEqual(response.content.decode(), expected_html)
 
+class TaskModelTest(TestCase):
+	def test_saving_and_retrieving_tasks(self):
+		first_task = Task()
+		first_task.text = 'first task'
+		first_task.save()
+
+		second_task = Task()
+		second_task.text = 'second task'
+		second_task.save()
+
+		saved_tasks = Task.objects.all()
+		self.assertEqual(saved_tasks.count(), 2)
+
+		first_saved_task = saved_tasks[0]
+		second_saved_task = saved_tasks[1]
+
+		self.assertEqual(first_saved_task.text, 'first task')
+		self.assertEqual(second_saved_task.text, 'second task')
