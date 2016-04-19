1a487a9b63912aa6dbb008c40a84f50ff78d79fe
amazingguni <amazingguni@gmail.com>
2016-04-19 23:26:27 +0900

    list view를 리팩터링해서 신규 아이템 POST를 처리하도록 함

--- lists/urls.py
+++ lists/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
     url(r'^new$', 'lists.views.new_list', name='new list'),
     #url(r'^admin/', admin.site.urls),
 ]
