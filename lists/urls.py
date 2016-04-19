fb0b4905c33524ef1b1287c8bfaf65183005476d
amazingguni <amazingguni@gmail.com>
2016-04-19 23:41:07 +0900

    템플릿에 있는 하드코딩된 URL 부분 리팩터링

--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^new$', 'lists.views.new_list', name='new list'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
     #url(r'^admin/', admin.site.urls),
 ]
